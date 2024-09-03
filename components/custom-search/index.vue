6a70b335132a7a5a44399e4e3c4032a1b80025a7
chenbitao <cbtpro@163.com>
2024-09-03 10:43:07 +0800

    refactor: 使用 defineModel 重构代码

--- components/custom-search/index.vue
+++ components/custom-search/index.vue
@@ -5,33 +5,16 @@
 <script setup lang="ts">
 import { debounce } from '@/utils/perf';
 
-interface IProps {
-  modelValue?: string;
-}
-
 defineOptions({
   name: 'CustomSearch'
 })
 
-const props = withDefaults(defineProps<IProps>(), {
-  modelValue: '',
-})
+const model = defineModel({ type: String })
 
-const emit = defineEmits<{
-  'update:modelValue': [value: string]
-}>()
-
-const { modelValue } = toRefs(props)
-const searchKey = ref(modelValue.value)
-
-watch(modelValue, (value, oldValue) => {
-  if (value !== oldValue) {
-    searchKey.value = value
-  }
-})
+const searchKey = ref(model.value)
 
 const onSearchHandle = debounce((value: string) => {
-  emit('update:modelValue', value)
+  model.value = value
 }, 300)
 
 </script>
